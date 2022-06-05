56ffd2e60fe72317cfbe2d40e99ff20e84ae99d6
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2022-06-05 12:33:25 +0300

    refactor: disable and remove request specs

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -4,5 +4,7 @@ Rails.application.config.generators do |g|
   g.test_framework :rspec,
     fixtures: false,
     view_specs: false,
-    helper_specs: false
+    helper_specs: false,
+    routing_specs: false,
+    request_specs: false
 end
